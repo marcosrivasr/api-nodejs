d4fb50b48ba584d3ee123826163b986c1dba2f70
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-05-20 16:39:46 -0500

    get users created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../model/usermodel');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  
+  let results;
+
+  try{
+    results = await User.find({}, '_id username password');
+  }catch(ex){
+    next()
+  }
+
+  res.json(results);
 });
 
 router.get('/:id_user', function(req, res, next) {
