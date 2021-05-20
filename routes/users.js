a640e2872f71e0967ce59f9034b91c7443ae4b18
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-05-20 16:40:32 -0500

    get specific user created

--- routes/users.js
+++ routes/users.js
@@ -2,32 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', async function(req, res, next) {
-  console.log(req.body);
-  const {username, password} = req.body;
-
-  if(!username || !password){
-    next();
-  }else{
-    const user = new User({username, password});
-
-    const exists = await user.usernameExists(username);
-
-    if(exists){
-      res.json({
-        message: 'user exists'
-      });
-    }else{
-      await user.save();
-
-      console.log('User added');
-      res.json({
-        message: 'User added'
-      });
-    }
-    
-  }
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
+
+router.get('/:id_user', function(req, res, next) {
+  res.json({
+router.get('/:iduser', async function(req, res, next) {
+  let results;
+
+  try{
+    results = await User.findById(req.params.iduser, '_id username');
+  }catch(ex){
+    next()
+  }
+
+  res.json(results);
+});
+
 });
 
 module.exports = router;
