013907e6f993f8bfdd79d0ad41a6f3d64cc0378f
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-05-21 20:22:39 -0500

    products rote changed

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/', indexRouter);
 app.use('/api/auth', usersRouter);
 app.use('/api/orders', usersRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/products', usersRouter);
+app.use('/api/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
