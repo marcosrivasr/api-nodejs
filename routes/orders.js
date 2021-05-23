212a5cf4855a66ec8382f46250a4c9503c8e6185
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-05-23 16:28:30 -0500

    post route for adding new order

--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const Order = require('../model/ordermodel');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/lol', function(req, res, next) {
-  res.send('lol');
+router.post('/', async function(req, res, next) {
+  const {iduser, products} = req.body;
+
+  if(!iduser || !products){
+    console.log('error');
+    next();
+  }else{
+    const order = new Order({iduser, products});
+    try{
+      await order.save();
+
+      res.json({
+        message: 'Order added'
+      });
+    }catch(ex){
+      req.error = ex.message;
+      next();
+    }
+
+    
+  }
 });
 
 module.exports = router;
